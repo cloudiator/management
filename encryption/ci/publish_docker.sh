957e02863e6cdf0a95f7419d430bcc52afc0e849
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-05 13:01:49 +0200

    Fixed docker publish script

--- encryption/ci/publish_docker.sh
+++ encryption/ci/publish_docker.sh
@@ -3,7 +3,7 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=cloudiator/encryption-agent
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -t $REPO:$COMMIT encryption-agent
+docker build -t $REPO:$COMMIT encryption
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
